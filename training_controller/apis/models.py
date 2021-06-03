1c029b4c5d48e6abd0d9aaa8f2a6c5c3e51f0b13
mainadwitiya <adw.tri2012@gmail.com>
2021-06-03 17:09:17 +0530

    api-fix

--- training_controller/apis/models.py
+++ training_controller/apis/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 import string,random
 import os
-import tensorflow as tf
-from google.protobuf import text_format
-from object_detection.protos import pipeline_pb2
+
+
 
 # Create your models here.
 
@@ -26,30 +25,28 @@ from object_detection.protos import pipeline_pb2
 #         return model_id
 
   
-def user_directory_path(model_id):
-    return 'post_data/'+str(model_id)
+def user_directory_path(usecase_type):
+    return 'post_data/'+str(usecase_type)
 
 
-def create_path(instance,model_id):
-    print(model_id)
+def create_path(instance,usecase_type):
+    print(usecase_type)
     #print(os.path.join('post_data',model_id))
-    return os.path.join('post_data',model_id)
+    return os.path.join('post_data',usecase_type)
 
 def group_based_upload_to(instance, filename):
-    return "post_data/{}/{}".format(instance.model_id, filename)
+    return "post_data/{}/{}".format(instance.usecase_type, filename)
 
     
 class Model_Post(models.Model):
     
-    
-    
-    model_id = models.CharField(max_length=32)
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    model_type=models.CharField(max_length=100)
-    
+    #model_id = model_type=models.CharField(max_length=100)
+    usecase_type = models.CharField(max_length=100)
+    framework_type = models.CharField(max_length=100)
+    model_arch_type =models.CharField(max_length=100)
     training_data = models.FileField(upload_to=group_based_upload_to)
     test_data = models.FileField(upload_to=group_based_upload_to)
     label_file_data= models.FileField(upload_to=group_based_upload_to)
+    
     def __str__(self):
         return self.title
--- training_controller/apis/views.py
+++ training_controller/apis/views.py
@@ -10,6 +10,13 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import FileResponse
+import argparse
+
+# import tensorflow as tf
+# from google.protobuf import text_format
+# from object_detection.protos import pipeline_pb2
+
+
 # Create your views here.
 
 # def effed(request):
@@ -25,10 +32,31 @@ from django.http import FileResponse
 # class ModelView(generics.CreateAPIView):
 #     queryset= Model_Data.objects.all()
 #     serializer_class = Model_Data_Serializer
-def response_data(model_type):
-    if model_type=='centernet':
-        file_path='apis/configs/tf2/centernet_hourglass104_512x512_coco17_tpu-8.config'
-    return file_path
+
+def edit_data(config_file_path):
+    pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
+    print(pipeline_config)
+    with tf.io.gfile.GFile(file_path, "r") as f:                                                                                                                                                                                                                     
+        proto_str = f.read()                                                                                                                                                                                                                                          
+        text_format.Merge(proto_str, pipeline_config) 
+    pipeline_config.train_input_config.label_map_path= "123/label_map.txt"
+    pipeline_config.model.ssd.image_resizer.fixed_shape_resizer.height = 300                                                                                                                                                                                          
+    pipeline_config.model.ssd.image_resizer.fixed_shape_resizer.width = 300
+
+    config_text = text_format.MessageToString(pipeline_config)                                                                                                                                                                                                        
+    with tf.io.gfile.GFile(path_2, "wb") as f:                                                                                                                                                                                                                       
+        f.write(config_text)  
+
+    
+    
+    
+
+def response_data(model_arch_type):
+    if model_arch_type=='centernet':
+        config_file_path='apis/configs/tf2/centernet_hourglass104_512x512_coco17_tpu-8.config'
+    elif model_arch_type=='fasterrcnn':
+        config_file_path='apis/configs/tf2/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.config'
+    return config_file_path
 class PostView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
@@ -46,16 +74,16 @@ class PostView(APIView):
         if posts_serializer.is_valid():
             #print(posts_serializer)
             posts_serializer.save()
-            print(posts_serializer.data['model_type'])
+            print(posts_serializer.data['model_arch_type'])
             
-            xxx=response_data(posts_serializer.data['model_type'])
+            xxx=response_data(posts_serializer.data['model_arch_type'])
             img = open(xxx, 'rb')
             response = FileResponse(img)
             print(response)
             print('Sahi se upload hui image')
             
-            #return Response(posts_serializer.data, status=status.HTTP_201_CREATED)
-            return response
+            return Response(posts_serializer.data, status=status.HTTP_201_CREATED)
+            #return response
             
         else:
             print('error', posts_serializer.errors)
